60e9197cedcf4d8968de4f5c7502634257553074
MadsPrestonBC <maddyprestn@gmail.com>
2025-10-19 00:55:39 -0400

    print statement

--- records-request.py
+++ records-request.py
@@ -326,7 +326,6 @@ class TherapyAppointmentBot:
 
         # Search for the line containing "School District"
         if "\nSchool District" in full_text:
-            print(full_text)
             match = re.search(r"School District\r?\n(.+)", full_text)
             match = match.group(1).strip()
         elif "to receive educational records from:" in full_text:
